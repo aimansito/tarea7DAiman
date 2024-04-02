6dde6c4dc1b0c4d2164d2f339e5ee59399159ba1
Aiman <ahardao1001@g.educaand.es>
2024-04-02 10:52:34 +0200

    primer commit

---
+++ src/main/java/com/mycompany/tarea7daiman/Tarea7DAiman.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea7daiman;
+
+/**
+ *
+ * @author aiman
+ */
+public class Tarea7DAiman {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
