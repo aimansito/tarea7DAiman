a19fe46afcefe84673dc82e42f75c5136416131c
Aiman <ahardao1001@g.educaand.es>
2024-04-03 19:46:02 +0200

    ejercicio hecho

--- src/main/java/daw/App.java
+++ src/main/java/daw/App.java
@@ -3,14 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package daw;
-
+import java.time.LocalDate;
+import java.text.DecimalFormat;
 import java.util.Random;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import org.apache.commons.lang3.RandomStringUtils;
 
 /**
  *
  * @author aiman
  */
+// Anotación @XmlRootElement, nombre de la etiqueta XML raíz.
+@XmlRootElement(name = "factura")
+// Anotación @XmlAccesorType define el elemento que usará JAXB durante el 
+// procesamiento de datos (en este caso por atributo)
+@XmlAccessorType(XmlAccessType.FIELD)
 public class App {
     private int codApp; 
     private String nombre;
@@ -28,10 +40,14 @@ public class App {
     }
 
     public App() {
+       DoubleStream tamañoKB = new Random().doubles(1,100, 1000);
+       IntStream numeroDescargas = new Random().ints(0, 50000);
        contador++;
        this.codApp = contador;
        this.nombre = "app"+contador+RandomStringUtils.randomAlphabetic(1);
        this.descripcion = descripcionApp();
+       this.tamaño = tamañoKB.findFirst().getAsDouble();
+       this.numDescargas = numeroDescargas.findFirst().getAsInt();
        
     }
     public static String descripcionApp(){
@@ -52,6 +68,7 @@ public class App {
         
         return descripciones[num];
     } 
+    
 
     public int getCodApp() {
         return codApp;
@@ -77,17 +94,26 @@ public class App {
         return contador;
     }
 
+    public String toCSV(){
+        return this.codApp+","+this.nombre+","+this.descripcion+","
+                +this.tamaño+","+this.numDescargas;
+    }
+    public String toTXT(){
+        return this.codApp+";"+this.nombre+";"+this.descripcion+";"
+                +this.tamaño+";"+this.numDescargas;
+    }
+    
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        DecimalFormat df = new DecimalFormat("#.##");
         sb.append("App{");
         sb.append("codApp=").append(codApp);
-        sb.append(", nombre=").append(nombre);
-        sb.append(", descripcion=").append(descripcion);
-        sb.append(", tama\u00f1o=").append(tamaño);
-        sb.append(", numDescargas=").append(numDescargas);
+        sb.append("; nombre=").append(nombre);
+        sb.append("; descripcion=").append(descripcion);
+        sb.append("; tamaño=").append(df.format(tamaño));
+        sb.append("; numDescargas=").append(numDescargas);
         sb.append('}');
         return sb.toString();
     }
-    
 }
